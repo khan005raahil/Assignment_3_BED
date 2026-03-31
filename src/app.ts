72648541da1bc31b3c49042000a28e9f3dac27fb
khan005raahil <rkhan2@rrc.ca>
2026-03-30 19:55:48 -0500

    added helmet config and cors config.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import express, {Express} from 'express';
 import morgan from 'morgan';
 import healthRouter from './api/v1/routes/healthRoutes';
 import eventRouter from "./api/v1/routes/eventRoutes";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+import {getHelmetConfig} from "../config/helmetConfig";
+import {getCorsOptions} from "../config/corsConfig";
+dotenv.config();
 
 const app: Express = express();
+app.use(getHelmetConfig());
+app.use(cors(getCorsOptions()));
 app.use(express.json());
 app.use(morgan('combined'));
 app.use('/api/v1/health', healthRouter);
